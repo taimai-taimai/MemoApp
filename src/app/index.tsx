cd2a5ebe8de353cf562c71b4bedba9862af8206a
taimai-taimai <204948513+taimai-taimai@users.noreply.github.com>
2025-07-03 07:05:32 +0900

    expo-rooter対応

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,46 @@ import { View, Text } from 'react-native';
 const Index = (): React.ReactElement => {
     return (
         <View>
-            <Text>Hello World</Text>
+            
+            <View>
+                <View>
+                    <Text>Memo AApp</Text>
+                    <Text>ログアウト</Text>
+                </View>
+            </View>
+            <View> 
+
+                <View>
+                    <View>買い物リスト</View>
+                    <View>2024年07月02日 10:00</View>
+                </View>
+                <View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+                <View>
+                    <View>買い物リスト</View>
+                    <View>2024年07月02日 10:00</View>
+                </View>
+                <View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+                <View>
+                    <View>test</View>
+                    <View>2024年07月02日 10:00</View>
+                </View>
+                <View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+            </View>
         </View>
     )
 }
