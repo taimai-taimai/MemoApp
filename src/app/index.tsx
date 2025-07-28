df9c301181bf0e8a044bd46d7a9d1c1acda1ce3e
taimai-taimai <204948513+taimai-taimai@users.noreply.github.com>
2025-07-29 08:25:06 +0900

    header style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,12 +5,15 @@ const Index = (): React.ReactElement => {
     return (
         <View style={styles.container}>
             
-            <View>
-                <View>
-                    <Text>Memo App</Text>
-                    <Text>ログアウト</Text>
+            <View style={styles.header}>
+
+                <View style={styles.headerInner}>
+                    <Text style={styles.headerTitle}>Memo App</Text>
+                    <Text style={styles.headerRight}>ログアウト</Text>
                 </View>
+
             </View>
+
             <View> 
 
                 <View>
@@ -60,8 +63,28 @@ const Index = (): React.ReactElement => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        backgroundColor: '#ffffff',
+    },
+    header: {
+        backgroundColor: '#467FD3',
+        height: 104,
+        justifyContent: 'flex-end',
+    },
+    headerInner: {
         alignItems: 'center',
+    },
+    headerTitle: {
+        marginBottom: 8,
+        fontSize: 22,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        color: '#ffffff',
+    },
+    headerRight: {
+        position: 'absolute',
+        right: 16,
+        bottom: 16,
+        color: 'rgba(255, 255, 255, 0.7)',
     }
 })
 
