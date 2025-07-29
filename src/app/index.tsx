9c17ff0cc2b502a2b9c679795e5675c7fa6317f1
taimai-taimai <204948513+taimai-taimai@users.noreply.github.com>
2025-07-29 11:54:18 +0900

    component化

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,56 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Header from '../components/Header';
+import MemoListItem from '../components/MomoListItem';
+import CircleButton from '../components/CircleButton';
 
 const Index = (): React.ReactElement => {
     return (
         <View style={styles.container}>
-            
-            <View style={styles.header}>
-
-                <View style={styles.headerInner}>
-                    <Text style={styles.headerTitle}>Memo App</Text>
-                    <Text style={styles.headerRight}>ログアウト</Text>
-                </View>
-
+            <Header />
+            <View>
+                <MemoListItem />
+                <MemoListItem />
+                <MemoListItem />
             </View>
-
-            <View> 
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2024年07月02日 10:00</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2024年07月02日 10:00</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2024年07月02日 10:00</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
-            </View>
-
-            <View style={styles.circleButton}>
-                <Text style={styles.circleButtonLabel}>+</Text>
-            </View>
+            <CircleButton>+</CircleButton>
 
         </View>
     )
@@ -61,67 +24,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#ffffff',
     },
-    header: {
-        backgroundColor: '#467FD3',
-        height: 104,
-        justifyContent: 'flex-end',
-    },
-    headerInner: {
-        alignItems: 'center',
-    },
-    headerTitle: {
-        marginBottom: 8,
-        fontSize: 22,
-        lineHeight: 32,
-        fontWeight: 'bold',
-        color: '#ffffff',
-    },
-    headerRight: {
-        position: 'absolute',
-        right: 16,
-        bottom: 16,
-        color: 'rgba(255, 255, 255, 0.7)',
-    },
-    memoListItem: {
-        backgroundColor: '#ffffff',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingVertical: 16,
-        paddingHorizontal: 19,
-        alignItems: 'center',
-        borderBottomWidth: 1,
-        borderBottomColor: 'rgba(0, 0, 0, 0.15)',
-    },
-    memoListItemTitle: {
-        fontSize: 16,
-        lineHeight: 32,
-    },
-    memoListItemDate: {
-        fontSize: 12,
-        lineHeight: 16,
-        color: '#848484',
-    },
-    circleButton: {
-        width: 64,
-        height: 64,
-        borderRadius: 32,
-        backgroundColor: '#467FD3',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        right: 40,
-        bottom: 40,
-        shadowColor: '#000000',
-        shadowOpacity: 0.25,
-        shadowRadius: 8,
-        shadowOffset: { width: 0, height: 8 },
-        elevation: 8,
-    },
-    circleButtonLabel: {
-        color: '#ffffff',
-        fontSize: 40,
-        lineHeight: 40,
-    }
+
 })
 
 export default Index;
